8a26fff789f7fe387ef2a6199c49838a85ecc576
geomagg <154397450+geomagg@users.noreply.github.com>
2024-09-28 18:03:22 -0300

    adjusting the EBCDIC IO

--- TextHeader_0.1.py
+++ TextHeader_0.1.py
@@ -6,6 +6,8 @@ from segysak.segy import create_default_texthead
 from segysak.segy import put_segy_texthead
 from segysak.segy import get_segy_texthead
 
+from CTkMessagebox import CTkMessagebox
+
 #from parse_text_header import parse_text_header  
 import matplotlib.pyplot as plt
 import pathlib
@@ -15,9 +17,21 @@ root.title("SEGY TEXT HEADER EDITOR")
 
 def new_file():
     text.delete("1.0", tk.END)
-    root.title("SEGY Text Header Editor")
+    root.title("SEGY Header Manipulation")
+
+def open_file():
+    text.delete("1.0", tk.END)
+    file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            text.delete("1.0", tk.END)
+            text.insert(tk.END, content)
+        root.title(file_path)
+
 
 def open_segy():
+    text.delete("1.0", tk.END)
     global ebcdic, filesegy
     file_segy = filedialog.askopenfilename(defaultextension=".segy", filetypes=[("SEGY FILES", "*.sgy"), ("All Files", "*.*")])
     filesegy = pathlib.Path(file_segy)
@@ -32,19 +46,22 @@ def save_file():
         with open(file_path, 'w') as file:
             content = text.get("1.0", tk.END)
             file.write(content)
+            CTkMessagebox(title="Saved File", icon="check.png",
+                  message="EBCDIC FILE saved:")
+            text.delete("1.0", tk.END)
         root.title(file_path)
 
         
 def save_segy():
-    file_path = filedialog.asksaveasfilename(defaultextension=".sgy", filetypes=[("SGY Files", "*.segy"), ("All Files", "*.*")])
-    if file_path:
-        with open(file_path, 'w') as file:
-            content= put_segy_texthead(filesegy, ebcdic)
-            file.write(filesegy)
-#            print (get_segy_texthead(filesegy))
-        root.title(file_path)
+    CTkMessagebox(title="Saving pad into Segy file header", icon="check.png",
+                  message="Are yiu sure?:")
+    print ("PORAAAAAAAA")
+    put_segy_texthead(filesegy, ebcdic)
 
+    root.title("Saving in SEGY TEXT HEADER")
 
+def clean():
+    text.delete("1.0", tk.END)
 
 
 text = tk.Text(root, wrap="word", undo=True)
@@ -55,15 +72,18 @@ root.config(menu=menu_bar)
 
 file_menu = tk.Menu(menu_bar, tearoff=0)
 
-menu_bar.add_cascade(label="SEGY IO", menu=file_menu)
-file_menu.add_command(label="Open", command=open_segy)
-file_menu.add_command(label="Save EBCDIC HEADER", command=save_file)
-file_menu.add_command(label="Save SEGY FILE", command=save_segy)
+menu_bar.add_cascade(label="EBCDIC", menu=file_menu)
+file_menu.add_command(label="Extract EBCDIC from segy file", command=open_segy)
+file_menu.add_command(label="Read EBCDIC from a file", command=open_file)
+file_menu.add_command(label="Save EBCDIC header into a file", command=save_file)
+file_menu.add_command(label="Update EBCDIC in cuurent segy", command=save_segy)
 file_menu.add_separator()
+file_menu.add_command(label="Clear pad", command=clean)
 file_menu.add_command(label="Exit", command=root.destroy)
 
+
 file_menu1 = tk.Menu(menu_bar, tearoff=0)
-#menu_bar.add_cascade(label="EBCDIC HEADER", menu=file_menu1)
+menu_bar.add_cascade(label="BINARY HEADER", menu=file_menu1)
 #file_menu1.add_command(label="Open", command=open_file)
 #file_menu1.add_command(label="Save", command=save_file)
 
